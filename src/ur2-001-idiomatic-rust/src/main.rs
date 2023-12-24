da0b4f0d002accd80878809781e5e90a9b8fc78c
Petrik van der Velde <petrikvandervelde@gmail.com>
2023-12-25 09:33:45 +1300

    Add lesson 1 for the Ultimate Rust 2 tutorials

---
+++ src/ur2-001-idiomatic-rust/src/main.rs
@@ -0,0 +1,37 @@
+// 1. This code looks terrible. Let's start cleaning this up by running `cargo fmt`. If you
+// configured your editor or IDE to run `cargo fmt` automatically upon save, you can just save!
+
+// 2. `cargo fmt` is great, but it doesn't add blank lines where there are none. Go ahead and add
+// some blank lines in places you think it would make sense.
+
+// 3. Time to clean up! Run `cargo clippy`. Fix up all the warnings so `cargo clippy` is silent.
+
+// Challenge: Clippy doesn't find *everything*. What else would you change to make this code better?
+
+fn count_to_5() -> i32 {
+    let mut val = 0;
+    loop {
+        if val > 5 {
+            break;
+        }
+
+        val += 1;
+    }
+
+    5
+}
+
+fn main() {
+    println!("I can count to {}", count_to_5());
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn test_counting() {
+        assert_eq!(count_to_5() == 5, true);
+    }
+}
