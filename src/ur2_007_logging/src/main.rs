ecb3508ff5af22160c889429925cffdd95bd3db6
Petrik van der Velde <petrikvandervelde@gmail.com>
2023-12-25 09:35:36 +1300

    Add lesson 7 for the Ultimate Rust 2 tutorials

---
+++ src/ur2_007_logging/src/main.rs
@@ -0,0 +1,27 @@
+// START IN lib.rs!!!
+
+use ur2_007_logging::Frog;
+
+// You did #1-#6 in lib.rs already, right?
+//
+// 7. Update Cargo.toml to add the `env_logger` dependency
+
+fn main() {
+    // 8. Initialize env_logger using the init() function at the top level of the library
+    env_logger::init();
+
+    // 9. Run this program with `cargo run` and take a look at the default output.
+    // - Now run it again with an explicit log level, like `RUST_LOG=info cargo run`
+    // - or `RUST_LOG=trace cargo run`
+    let mut skippy = Frog::new();
+    skippy.hop();
+    skippy.hop();
+    skippy.hop();
+    skippy.hop();
+    skippy.hop();
+    skippy.sleep();
+    skippy.sleep();
+
+    // Challenge: Go back to lib.rs and set the `target: ` argument for each logging call to be the
+    // path to the function.  For example, `Frog::new`
+}
